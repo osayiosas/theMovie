9317b0134dc00be0889584166f4bfc5b25c4b507
osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2023-09-20 15:03:24 +0100

    added the api

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React from "react"; 
 import image from '../image/poster.jpg'
 
-const Card = () => {
+
+const Card = (movie) => {
+
+  console.log(movie.info);
+
+  let img_path = "https://image.tmdb.org/t/p/w500";
+
   return (
     <div>
       <div className="movie">
         <img
-          src={image} alt="poster"
-         width={250}></img>
+          src={img_path + movie.info.poster_path}
+          className="poster"
+          alt="poster"
+        ></img>
 
         <div className="movie-details">
           <div className="box">
-            <h4 className="title">Movie Title</h4>
-            <p className="rating" >9.7</p>
+            <h4 className="title">{movie.info.title}</h4>
+            <p className="rating">{movie.info.vote_average}</p>
           </div>
 
           <div className="overview">
             <h1>Overview</h1>
-            mathe this a beatiful movies to watch out for.
+            {movie.info.overview}
           </div>
         </div>
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 
+let API_key = "&api_key=8f6498271492664a31219c6324547290";
+let base_url = "https://api.themoviedb.org/3";
+let url=base_url+"/discover/movie?sort_by=popularity.desc"+API_key;
+let arr = ["Popular", "Theater", "Kids", "Drama", "Comedie"];
+
+
+
 const Main = () => {
+
+  const [movieData, setData]=useState([]);
+  const [url_set, setUrl]=useState(url);
+  const [search, setSearch] = useState();
+
+  useEffect(() =>{
+    fetch(url_set).then(res=>res.json()).then(data=> {
+      setData(data.results)
+    })
+  }, [url_set])
+
+  const getDate=(movieType) => {
+    if(movieType=='Popular') {
+      url = base_url + "/discover/movie?sort_by=popularity.desc" + API_key;
+    }
+    if (movieType == "Theatre") {
+      url =
+        base_url +
+        "/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22" +
+        API_key;
+    }
+        if (movieType == "Kids") {
+          url =
+            base_url +
+            "/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc" +
+            API_key;
+        }
+        if (movieType == "Drama") {
+          url =
+            base_url +
+            "/discover/movie?with_genres=18&primary_release_year=2014" +
+            API_key;
+        }
+        if (movieType == "Comedie") {
+          url =
+            base_url +
+            "/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc" +
+            API_key;
+        }
+        setUrl(url);
+
+    }
+
+    const searchMovie=(evt)=> {
+      if(evt.key=="Enter")
+       {
+       url =
+         base_url +
+         "/search/movie?api_key=8f6498271492664a31219c6324547290&query=" +
+         search;
+       setUrl(url);
+       setSearch(" ");
+      }
+    };
+  
+
   return (
     <>
       <div className="header">
         <nav>
           <ul>
-            <li>
-              <a href="#">Popular</a>
-            </li>
-            <li>
-              <a href="">Theater</a>
-            </li>
-            <li>
-              <a href="#">Kids</a>
-            </li>
-            <li>
-              <a href="#">Drama</a>
-            </li>
-            <li>
-              <a href="#">Comedie</a>
-            </li>
+
+            {
+              arr.map((value) =>{
+                return (
+                  <li><a href="#" name={value} onClick={(e)=>{getDate(e.target.name)}}>{value}</a></li>
+                )
+              })
+            }
           </ul>
         </nav>
 
@@ -30,21 +86,22 @@ const Main = () => {
             <input
               type="text"
               placeholder="Enter Movie Name"
-              className="inputText"
-            ></input>
+              className="inputText" onChange={(e) =>{setSearch(e.target.value)}} value={search} onKeyDown={searchMovie}></input>
             <button>
-              <i class="fa-brands fa-searchengin"></i>
+              <i className="fa-brands fa-searchengin"></i>
             </button>
           </div>
         </form>
       </div>
 
       <div className="container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {
+          (movieData.length==0)?<p className="notfound">Not Found</p>: movieData.map((res, pos)=>{
+            return(
+              <Card info={res} key={pos} />
+            )
+          })
+        }
       </div>
     </>
   );
